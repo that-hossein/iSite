12171423de0314171ac1f0a95d1dd78d25f116ff
androsein <androsein@yahoo.com>
2020-06-14 23:31:31 +0430

    Install bulma    - import in main.js to affects on all parts    - fix custom font (index.css) problem with bulma

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
+import "./../node_modules/bulma/css/bulma-rtl.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
