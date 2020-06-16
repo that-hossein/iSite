5fba5176afb78b9636ad033b8691f62fd6182bb7
androsein <androsein@yahoo.com>
2020-06-16 10:09:46 +0430

    Add post list in home page with sample data

---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="column is-one-third">
+    <div class="post-item">
+      <div class="frame">
+        <img :src="post.indicatorImage" alt />
+      </div>
+      <h4 class="title is-5">{{post.title}}</h4>
+      <p class="summary">{{post.body}}</p>
+      <div class="info-container">
+        <i data-feather="calendar"></i>
+        <span>{{post.date}}</span>
+        <i data-feather="clock" class="mr-4"></i>
+        <span>{{post.time}}</span>
+        <i data-feather="message-square" class="mr-4"></i>
+        <span>{{post.commentsCount}}</span>
+      </div>
+    </div>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    post: Object
+  }
+};
+</script>
---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <transition-group name="list" tag="div" class="columns is-multiline">
+        <Post v-for="post in posts" :key="post.id" :post="post" />
+      </transition-group>
+    </div>
+  </section>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  components: {
+    Post
+  },
+  props: {
+    posts: Array
+  }
+};
+</script>
+
+<style scoped>
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter,
+.list-leave-to {
+  /* .list-leave-active below version 2.1.8 */
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
